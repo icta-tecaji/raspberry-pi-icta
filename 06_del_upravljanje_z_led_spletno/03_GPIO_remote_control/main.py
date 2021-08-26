7afb9b0a453986659aa400d463f2049e35a78573
Balki <balkovec.gregor@gmail.com>
2021-08-26 17:08:13 +0200

    06 done

--- 06_del_upravljanje_z_led_spletno/03_GPIO_remote_control/main.py
+++ 06_del_upravljanje_z_led_spletno/03_GPIO_remote_control/main.py
@@ -4,10 +4,16 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-pins = {"1": 17,
-        "2": 27}
+pins = {"1": {
+		"pin_num": 17,
+		"state": "off"
+		},
+        "2": {
+		"pin_num": 27,
+		"state": "off" }}
 for pin in pins:
-    GPIO.setup(pins[pin], GPIO.OUT)
+	GPIO.setup(pins[pin]["pin_num"], GPIO.OUT)
+	GPIO.output(pins[pin]["pin_num"], GPIO.LOW)
 
 app = Flask(__name__)
 
@@ -18,32 +24,23 @@ def landing_page():
 
 @app.route("/interface")
 def interface_pins():
-    data = {}
-
-    if "1" not in request.args:
-        data["1"] = "off"
-    else:
-        data["1"] = request.args["1"]
-
-    if "2" not in request.args:
-        data["2"] = "off"
-    else:
-        data["2"] = request.args["2"]
-
-    control_pins(data)
-    
-    return render_template("main.html", data=data)
-	
+	print("Data: ", request.args)
+	for pin in pins:
+		if pin in request.args:
+			pins[pin]["state"] = request.args[pin]
+	data = pins
+	control_pins(data)
+	return render_template("main.html", data=data)
 
 
 def control_pins(data):
-    print("CONTROLING PINS: ", data)
-    for pin, action in data.items():
-        if pin in pins:
-            if action == "on":
-                GPIO.output(pins[pin_num], GPIO.HIGH)
-            else:
-                GPIO.output(pins[pin_num], GPIO.LOW)
+	print("CONTROLING PINS: ", data)
+	for pin, action in data.items():
+		if pin in pins:
+			if pins[pin]["state"] == "on":
+				GPIO.output(pins[pin]["pin_num"], GPIO.HIGH)
+			else:
+				GPIO.output(pins[pin]["pin_num"], GPIO.LOW)
 
 
 if __name__ == "__main__":
